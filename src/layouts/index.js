60ad7115fb88d4504319a4a3d1cca7ee74ea50ad
Matthew Turney <matthew.a.turney@gmail.com>
2018-03-20 22:39:15 -0500

    FontAwesome requires global CSS to be loaded into the client to function.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import fontawesome from '@fortawesome/fontawesome'
 import { compose, withProps } from 'recompose'
 import Helmet from 'react-helmet'
 import '../sass/global.scss'
@@ -26,6 +27,7 @@ const TemplateWrapper = ({ children, title, description, keywords = [] }) => (
         rel="stylesheet"
       />
       <link type="text/plain" rel="author" href="humans.txt" />
+      <style>{fontawesome.dom.css()}</style>
     </Helmet>
     {children()}
   </div>
